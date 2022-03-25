010c691aafb2a7d10a77df553c5022c68fe12b61
PatateNouille <hugo@kaxak.net>
2022-03-25 11:13:25 +0100

    Trying to customize response with php

--- test.php
+++ test.php
@@ -1,1 +1,15 @@
-response?
+<?php // ------ ERROR HANDLING
+
+if (!isset($_POST['content']))
+  die('Invalid request');
+
+?>
+
+
+<?php
+
+$content = $_POST['content'];
+
+echo 'Server received : '.$content.' !';
+
+?>
